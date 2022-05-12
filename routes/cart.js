edf14394e35ffd2bffd4ac860e57b2d340cf3cec
Shivanthi Fernando <wscsfernando@gmail.com>
2022-05-12 18:35:34 +0530

    create cart model

--- routes/cart.js
+++ routes/cart.js
@@ -1,7 +1,7 @@
+const Cart = require("../models/cart");
+
 const router = require("express").Router();
 
-router.get("/cart_test", (req, res) => {
-    res.send("Cart Test is Successful");
-});
+
 
 module.exports = router
