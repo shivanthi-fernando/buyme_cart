fac1ca7a73c06cd1aa59c1b32336cb5d23a23738
Shivanthi Fernando <wscsfernando@gmail.com>
2022-05-16 11:05:35 +0530

    Create Cart and other changes

--- routes/cart.js
+++ routes/cart.js
@@ -1,7 +1,16 @@
 const Cart = require("../models/cart");
-
 const router = require("express").Router();
 
+//Create Cart
+router.post("/", async(req, res) => {
+    const newCart = new Cart(req.body);
 
+    try{
+        const savedCart = await newCart.save();
+        res.status(200).json(savedCart);
+    }catch (err){
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router
